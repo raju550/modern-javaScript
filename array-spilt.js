4b62672b9d876e6063a9c3e1c82c3701019f2164
Md Ismail Hossain <ismailtex043@gmail.com>
2021-07-10 21:35:54 +0600

    slice splice

---
+++ array-spilt.js
@@ -0,0 +1,4 @@
+const numbers = [2, 4, 9, 7, 10, 5, 9, 8];
+// const result = numbers.slice(2, 5);
+const result = numbers.splice(2, 5);
+console.log(result);
---
+++ scope.js
@@ -0,0 +1,8 @@
+let bonus = 50;
+function sum(first, second) {
+  let result = first + second + bonus;
+  return result;
+}
+const value = sum(30, 50);
+console.log(bonus);
+console.log(value);
